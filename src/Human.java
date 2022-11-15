426aada623c8b5a15519d7f5b4e33136534c8e3b
Igoryok8 <energymag@mail.ru>
2022-11-16 02:24:10 +0300

    Задание 1 и 2 выполнены

--- src/Human.java
+++ src/Human.java
@@ -2,10 +2,12 @@ public class Human {
     String name;
     String town;
     int yearOfBirth;
+    String jobTitle;
+
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " +name+ ". Я из города "+town+ ". Я родился в " +yearOfBirth+ " году. Будем знакомы!";
+        return "Привет! Меня зовут " +name+ ". Я из города "+town+ ". Я родился в " +yearOfBirth+ " году. Я работаю на должности " +jobTitle+ ". Будем знакомы!";
 }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,34 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         System.out.println();
-
+        //Максим работает бренд-менеджером.
+          //      Аня работает методистом образовательных программ.
+        //Катя работает продакт-менеджером.
+          //      Артем работает директором по развитию бизнеса.
         Human maxim = new Human();
         maxim.name = "Максим";
         maxim.town = "Минск";
         maxim.yearOfBirth = 1988;
+        maxim.jobTitle = "Бренд-менеджер";
 
         Human anya = new Human();
         anya.name = "Аня";
         anya.town = "Москва";
         anya.yearOfBirth = 1993;
+        anya.jobTitle = "Методист образовательных программ";
 
         Human katya = new Human();
         katya.name = "Катя";
         katya.town = "Калининград";
         katya.yearOfBirth = 1992;
+        katya.jobTitle = "Продакт-менеджер";
 
         Human artyom = new Human();
         artyom.name = "Артём";
         artyom.town = "Москва";
         artyom.yearOfBirth = 1995;
+        artyom.jobTitle = "Директор по развитию бизнеса";
+
         System.out.println(maxim);
         System.out.println(anya);
         System.out.println(katya);
