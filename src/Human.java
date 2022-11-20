5f42fd81eff86d41d285685a0deb5b6ed73e0f62
Igoryok8 <energymag@mail.ru>
2022-11-21 00:11:59 +0300

    В классе Human созданы геттеры и сеттеры, создан новый объект Владимир. Создан новый класс Flower.java и выполнено задание по этому классу.

--- src/Human.java
+++ src/Human.java
@@ -1,27 +1,29 @@
 public class Human {
     String name;
-    String town;
-    int yearOfBirth;
+    private String town;
+    private int yearOfBirth;
     String jobTitle;
-    Human(String name,String town,int yearOfBirth,String jobTitle) {
 
-
+    Human(String name, String town, int yearOfBirth, String jobTitle) {
+        if (town == null || town.isEmpty() || town.isBlank()) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
         if (yearOfBirth < 0) {
             this.yearOfBirth = 0;
         } else {
             this.yearOfBirth = yearOfBirth;
         }
-        if (name == null|| name.isEmpty()||name.isBlank()){
+
+
+        if (name == null || name.isEmpty() || name.isBlank()) {
             this.name = "Информация не указана";
         } else {
             this.name = name;
         }
-        if (town ==null|| town.isEmpty()||town.isBlank()) {
-            this.town = "Информация не указана";
-        }else {
-            this.town = town;
-        }
-        if (jobTitle ==null|| jobTitle.isEmpty()||jobTitle.isBlank()) {
+
+        if (jobTitle == null || jobTitle.isEmpty() || jobTitle.isBlank()) {
             this.jobTitle = "Информация не указана";
         } else {
             this.jobTitle = jobTitle;
@@ -31,7 +33,27 @@ public class Human {
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " +name+ ". Я из города "+town+ ". Я родился в " +yearOfBirth+ " году. Я работаю на должности " +jobTitle+ ". Будем знакомы!";
-}
+        return "Привет! Меня зовут " + name + ". Я из города " + getTown()+ ". Я родился в " + getYearOfBirth() + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
+    public String getTown() {
+        return town;
+    }
+    public void setTown(String town) {
+        if (town == null || town.isEmpty() || town.isBlank()) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+    }
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
 
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,50 @@ public class Main {
         System.out.println("Задание 1");
         System.out.println();
 
-        Human maxim = new Human("  ", null,1988,"Бренд-менеджер");
-        Human anya = new Human("Аня","Москва",-1993,"Методист образовательных программ");
-        Human katya = new Human("Катя","Калининград",1992,null);
-        Human artyom = new Human(null,"Москва",1995,"Директор по развитию бизнеса");
+        Human maxim = new Human("Максим", null, 1988, "Бренд-менеджер");
+        Human anya = new Human("Аня", "Москва", -1993, "Методист образовательных программ");
+        Human katya = new Human("Катя", "Калининград", 1992, null);
+        Human artyom = new Human(null, "Москва", 1995, "Директор по развитию бизнеса");
+        Human vladimir = new Human("Владимир", "Казань", 2001, " ");
 
         System.out.println(maxim);
         System.out.println(anya);
         System.out.println(katya);
         System.out.println(artyom);
+        System.out.println(vladimir);
         System.out.println();
 
         System.out.println("Задание 2");
         System.out.println();
 
-        Car lada = new Car(null,"Granta",1.7,"жёлтый",2015,"в России");
+        Car lada = new Car(null, "Granta", 1.7, "жёлтый", 2015, "в России");
         System.out.println(lada);
 
-        Car audi = new Car("Audi","A8 50 L TDI quattro",-3.0,"чёрный",2020,"в Германии");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", -3.0, "чёрный", 2020, "в Германии");
         System.out.println(audi);
 
-        Car bmw = new Car("BMW"," ",3.0,"чёрный",2021,"в Германии");
+        Car bmw = new Car("BMW", " ", 3.0, "чёрный", 2021, "в Германии");
         System.out.println(bmw);
 
-        Car kia = new Car("Kia","Sportage 4-го поколения",2.4," ",-2018,null);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, " ", -2018, null);
         System.out.println(kia);
 
-        Car hyundai = new Car("Hyundai","Avante",1.6,null,2016,"в Южной Корее");
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, null, 2016, "в Южной Корее");
         System.out.println(hyundai);
-    }
+        System.out.println();
 
+        System.out.println("Задание 3");
+        System.out.println();
+
+        Flower rose = new Flower("Роза обыкновенная", "Голландия", 35.59,0 );
+        Flower chrysanthemum = new Flower("Хризантема", " ", 15, 5);
+        Flower pion = new Flower("Пион", "Англия", 69.9, 1);
+        Flower gypsophila = new Flower("Гипсофила", "Турция", 19.5, 10);
+
+        System.out.println(rose);
+        System.out.println(chrysanthemum);
+        System.out.println(pion);
+        System.out.println(gypsophila);
+        System.out.println();
+    }
 }
